d55fa190b7c6cfe59c2151194877e3d55137cb1a
hanagoku <108511558+hanagoku@users.noreply.github.com>
2022-07-08 18:10:13 +0900

    完了削除　ループ取得 07081810

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+import React, { useState } from "react";
+export const App = () => {
+  const [incompleteTODOS, setincompleteTODOS] = useState([
+    `おいおいおい`,
+    `うううううう`
+  ]);
+  const [completeTODOS, setCompleteTODOS] = useState(["おいおいおい"]);
+  <>
+    <div className="input-area">
+      <input placeholder="TODOを入力" />
+      <button>追加</button>
     </div>
-  );
+    <div className="incomplete-area">
+      <p className="title">未完了のTODO</p>
+      <ul>
+        {incompleteTODOS.map((TODO) => {
+          return (
+            <div key={TODO} className="list-row">
+              <li>{TODO}</li>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+      <ul>
+        {completeTODOS.map((TODO) => {
+          return (
+            <div key={TODO} className="list-row">
+              <li>{TODO}</li>
+              <button>戻す</button>
+            </div>
+            );
+            })}
+            </ul>
+            </>
 }
